414201b5be42ed41cfa8586699fe5953009e3344
Jeremy JC Ashley <113396637+Messyginger0804@users.noreply.github.com>
2025-09-10 19:00:09 +0000

    updated python version of Roman_Numeral_Parser

--- freeCodeCamp/Roman_Numeral_Parser/main.py
+++ freeCodeCamp/Roman_Numeral_Parser/main.py
@@ -1,3 +1,29 @@
 # Given a string representing a Roman numeral, return its integer value.
 
 # Numerals are read left to right. If a smaller numeral appears before a larger one, the value is subtracted. Otherwise, values are added.
+
+def parse_roman_numeral(numeral):
+    symbol_value = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    result = 0
+    last_value = 0
+
+    for symbol in reversed(numeral):
+        current_value = symbol_value[symbol]
+
+        if current_value < last_value:
+            result -= current_value
+        else:
+            result += current_value
+
+        last_value = current_value
+
+    return result
