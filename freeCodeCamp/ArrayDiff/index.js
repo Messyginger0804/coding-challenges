7f4b401ada5f9c6ca2973a1e431b7b80fff95016
Jeremy JC Ashley <113396637+Messyginger0804@users.noreply.github.com>
2025-09-10 14:57:13 +0000

    compelted arraydiff index.js

---
+++ freeCodeCamp/ArrayDiff/index.js
@@ -0,0 +1,30 @@
+// Given two arrays with strings values, return a new array containing all the values that appear in only one of the arrays.
+
+// The returned array should be sorted in alphabetical order.
+
+// function arrayDiff(arr1, arr2) {
+
+//     let answer = []
+
+//     for(let i = 0; i < arr1.length; i++){
+//         if(!arr2.includes(arr1[i])){
+//             answer.push(arr1[i])
+//         }
+//     }
+//     for(let j = 0; j < arr2.length; j++){
+//         if(!arr1.includes(arr2[j])){
+//             answer.push(arr2[j])
+//         }
+//     }
+
+//     answer.sort()
+
+//   return answer;
+// }
+
+const arrayDiff = (arr1, arr2) =>{
+    
+}
+
+
+arrayDiff(["apple", "banana"], ["apple", "banana", "cherry"])
