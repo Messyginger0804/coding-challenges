a29150d5cdbb2f3e3e75f14a7bb45ca1bfbecfcc
Jeremy JC Ashley <113396637+Messyginger0804@users.noreply.github.com>
2025-09-10 16:59:12 +0000

    updateing for unique

--- freeCodeCamp/Unique_Characters/index.js
+++ freeCodeCamp/Unique_Characters/index.js
@@ -2,19 +2,23 @@
 
 // Uppercase and lowercase letters should be considered different characters.
 
-function allUnique(str) {
-    const mySet = new Set()
+// function allUnique(str) {
+//     const mySet = new Set()
     
-    for(let char = 0; char < str.length; char++){
-        if(mySet.has(char)){
-            console.log(mySet)
-            return false
-        }else{
-            mySet.add(char)
-        }
-    }
+//     for(let char = 0; char < str.length; char++){
+//         if(mySet.has(char)){
+//             console.log(mySet)
+//             return false
+//         }else{
+//             mySet.add(char)
+//         }
+//     }
 
-  return true;
+//   return true;
+// }
+
+const allUnique = (str) => {
+    
 }
 
 allUnique('jesus')
