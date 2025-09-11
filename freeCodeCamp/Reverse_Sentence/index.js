afff6f33b06d67c8a341f1ed90e73d4fb0300a05
Jeremy JC Ashley <113396637+Messyginger0804@users.noreply.github.com>
2025-09-11 14:35:43 +0000

    completed Reverse_Sentence problem

---
+++ freeCodeCamp/Reverse_Sentence/index.js
@@ -0,0 +1,32 @@
+// # Reverse Sentence
+// # Given a string of words, return a new string with the words in reverse order. For example, the first word should be at the end of the returned string, and the last word should be at the beginning of the returned string.
+
+// # In the given string, words can be separated by one or more spaces.
+// # The returned string should only have one space between words.
+
+// function reverseSentence(sentence) {
+//     const splited = sentence.split(' ')
+//     const reversed = splited.reverse()
+//     const answer = reversed.reverse().join(' ')
+
+//     console.log(answer)
+//     return answer
+
+// }
+
+// reverseSentence("npm  install  sudo")
+
+function reverseSentence(sentence) {
+    const workdsWithGaps = sentence.split(' ')
+    const cleanWords = workdsWithGaps.filter(word => word !== '')
+
+    const reverseWords = cleanWords.reverse()
+
+    const result = reverseWords.join(' ')
+
+    console.log(result)
+    return result
+
+}
+
+reverseSentence("npm  install  sudo")
