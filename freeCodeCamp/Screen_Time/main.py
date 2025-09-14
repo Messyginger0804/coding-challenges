41ef25b2e9aa0d936751e7a67099c7cfd8fdfa04
Jeremy JC Ashley <113396637+Messyginger0804@users.noreply.github.com>
2025-09-14 15:43:28 +0000

    created python sript to fix it

--- freeCodeCamp/Screen_Time/main.py
+++ freeCodeCamp/Screen_Time/main.py
@@ -6,5 +6,23 @@
 # If the average of the seven days is greater than or equal to 6 hours, it's too much.
 
 def too_much_screen_time(hours):
+    if any(day >= 10 for day in hours):
+        print(True)
+        return True
+    
+    average = sum(hours) / len(hours)
+    if average >= 6:
+        print(True)
+        return True
 
-    return hours
+    for i in range(len(hours) - 2):
+        windowTotal = hours[i] + hours[i+1] + hours[i+2]
+        windowAverage = windowTotal / 3
+        if windowAverage / 3 >= 8:
+            print(True)
+            return True
+
+        
+    return False
+
+too_much_screen_time([3, 3, 5, 8, 8, 9, 4])
